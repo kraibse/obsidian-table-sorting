d2829a2a28ecf5164465dd5763e2a4469aca57af
kraibse <33869366+kraibse@users.noreply.github.com>
2023-02-11 23:52:17 +0100

    Added basic column sorting

--- main.ts
+++ main.ts
@@ -1,137 +1,145 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Plugin
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class TableSort extends Plugin {
+	statusBarItem: HTMLElement;
+	statusBarReload: HTMLElement;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+	const icons = {
+		unsorted: color => `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${color}">
+			<path d="M15 8H1l7-8zm0 1H1l7 7z" opacity=".2"/>
+		</svg>`,
+		ascending: color => `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${color}">
+			<path d="M15 8H1l7-8z"/>
+			<path d="M15 9H1l7 7z" opacity=".2"/>
+		</svg>`,
+		descending: color => `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${color}">
+			<path d="M15 8H1l7-8z" opacity=".2"/>
+			<path d="M15 9H1l7 7z"/>
+		</svg>`
+	};
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	lastSorted = {
+		columnIndex: 0,
+		isAscending: true
+	};
 
 	async onload() {
-		await this.loadSettings();
+		console.log(this.needsDarkTheme());
+		this.statusBarItem = this.addStatusBarItem();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const thead = document.querySelectorAll("th") as NodeListOf < HTMLTableCellElement > ;
+		thead.forEach((e) => {
+
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			if (evt.target == null) {
+				return;
+			}
+
+			const element: HTMLElement = evt.target;
+
+			if (element.tagName === "TH") {
+				evt.preventDefault();
+				this.sortTable(element);
+			}
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
+	sortTable(th: HTMLElement) {
+		const tableElement = th.closest("table");
+
+		if (tableElement == null) {
+			return undefined;
+		}
+
+		const thead = tableElement.querySelectorAll("th");
+
+		const rowElements = tableElement.querySelectorAll("tr");
+		const rows = Array.from(rowElements).splice(1, rowElements.length);
+
+		// Determine the index of the column to sort by
+		const sortColumnIndex = Array.prototype.indexOf.call(thead, th);
+
+		if (this.lastSorted.columnIndex === sortColumnIndex) {
+			this.lastSorted.isAscending = !this.lastSorted.isAscending;
+		} else if (this.lastSorted.columnIndex !== sortColumnIndex) {
+			this.lastSorted.isAscending = true;
+			this.lastSorted.columnIndex = sortColumnIndex;
+		}
+
+		this.statusBarItem.setText(th.getText());
+
+		// Sort the rows by the values in the sort column
+		const sortedRows = Array.from(rows).sort((rowA, rowB) => {
+			const cellA = rowA.children[sortColumnIndex] as HTMLTableCellElement;
+			const cellB = rowB.children[sortColumnIndex] as HTMLTableCellElement;
+
+			if (cellA == undefined || cellB == undefined) {
+				return 0;
+			}
+
+			const valueA = cellA.textContent ? .toLowerCase() ? ? false;
+			const valueB = cellB.textContent ? .toLowerCase() ? ? false;
+
+			if (this.lastSorted.isAscending) {
+				if (valueA < valueB) {
+					return -1;
+				} else if (valueA > valueB) {
+					return 1;
+				} else {
+					return 0;
+				}
+			} else {
+				if (valueA < valueB) {
+					return 1;
+				} else if (valueA > valueB) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+		});
+
+		// Remove all the rows from the table
+		Array.from(rows).forEach((row) => {
+			row.remove();
+		});
+
+		// Append the sorted rows to the table
+		sortedRows.forEach((row) => {
+			tableElement.querySelector("tbody").appendChild(row);
+		});
+	}
+
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	needsDarkTheme() {
+		// color will be "rgb(#, #, #)" or "rgba(#, #, #, #)"
+		let color = window.getComputedStyle(document.body).backgroundColor;
+		console.log("BG COLOR:", color);
+		
+		const rgb = (color || "")
+			.replace(/\s/g, "")
+			.match(/^rgba?\((\d+),(\d+),(\d+)/i);
+		if (rgb) {
+			// remove "rgb.." part from match & parse
+			const colors = rgb.slice(1).map(Number);
+			// http://stackoverflow.com/a/15794784/145346
+			const brightest = Math.max(...colors);
+			// return true if we have a dark background
+			return brightest < 128;
+		}
+		// fallback to bright background
+		return false;
 	}
 }
