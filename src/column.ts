38465e454fc68558e8d6fb0217e5dbd407822857
kraibse <33869366+kraibse@users.noreply.github.com>
2023-02-15 13:27:55 +0100

    Moved reset, setIcon, update to the column file

--- src/column.ts
+++ src/column.ts
@@ -1,9 +1,32 @@
 export class Column {
-    id: number;
-    mode: string;
+	id: number;
+	order: string;
+	element: HTMLElement;
 
-    constructor(id: number, mode: string) {
-        this.id = id;
-        this.mode = mode;
+	constructor(id: number, element: HTMLElement, order: string) {
+		this.id = id;
+		this.element = element;
+		this.order = order;
+	}
+
+	reset() {
+
+	}
+
+    setIcon() {
+        this.element.classList.remove("neutral");
+		this.element.classList.remove("ascending");
+		this.element.classList.remove("descending");
+
+        this.element.classList.add(this.order);
     }
+
+	update() {
+		/* Updates the order mode and updates column CSS class */
+        this.order = (this.order === "neutral") ? "descending"
+                    : (this.order === "ascending") ? "neutral"
+                    : "ascending";
+
+		this.setIcon();
+	}
 }
