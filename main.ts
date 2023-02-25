657f46f87485bb3c900ed80a0b3221c5e23a54da
kraibse <33869366+kraibse@users.noreply.github.com>
2023-02-25 02:04:09 +0100

    Extracted mosuedownHandler functionality to module

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Plugin } from 'obsidian';
 import { Generator } from '@types/core-js';
 
 import { Table } from "./src/table";
-import { Column } from "./src/column";
+import { getMousedownHandler } from "./src/mouseHandler"
 
 export default class TableSort extends Plugin {
 
@@ -16,70 +16,34 @@ export default class TableSort extends Plugin {
 		}
 	}
 
-	private getTableElement(th: HTMLElement): HTMLTableElement | undefined {
+	getTableElement(th: HTMLElement): HTMLTableElement | undefined {
 		return th.closest("table") || undefined;
 	}
-
-	private getTableID(table: HTMLElement): number {
-		const id = table.getAttribute("id")?.replace("table-", "");
+	
+	getTableID(table: HTMLElement): number {
+		const id = table.getAttribute("id") ?.replace("table-", "");
 		return (id) ? parseInt(id) : this.gen.next().value;
 	}
-
-	private hasCustomClasses(table: HTMLElement): boolean {
+	
+	hasCustomClasses(table: HTMLElement): boolean {
 		const classes = table.getAttribute("class") || "";
 		if (classes.length > 0) {
 			return true;
 		}
 		return false;
 	}
-
-	private isNewTable(id: number): boolean {
+	
+	isNewTable(id: number): boolean {
 		//  Return true if the user has selected a new table
 		return (this.storage.length - 1 >= id) ? false : true;
 	}
 
 	async onload() {
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.gen = this.autoIncrement();
 		this.storage = [];
 		
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			if (evt.target == null) {
-				return;
-			}
-			const element: HTMLElement = evt.target as HTMLElement;
-
-			if (element.tagName !== "TH") {
-				return;
-			}
-
-			evt.preventDefault();
-
-			const tableElement: HTMLTableElement | undefined = this.getTableElement(element);
-			if (!tableElement || this.hasCustomClasses(tableElement)) {
-				return;
-			}
-
-			const tableID: number = this.getTableID(tableElement);
-
-			let table;
-			if (this.isNewTable(tableID)) {
-				table = new Table(tableID, tableElement);
-				this.storage.push(table);
-			} else {
-				table = this.storage[tableID];
-			}
-
-			const columnIndex = table.getColumnIndex(element);
-			const column: Column = table.getColumnDataAt(columnIndex);
-
-			table.handleClick(column, evt.ctrlKey == true);
-			table.sort();
-		}, { capture: true });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		const mousedownHandler = getMousedownHandler(this);
+		this.registerDomEvent(document, 'click', mousedownHandler, { capture: true });
 	}
 
 	onunload() {
---
+++ src/mouseHandler.ts
@@ -0,0 +1,39 @@
+import TableSort from "../main";
+import { Table } from "./table";
+import { Column } from "./column";
+
+
+export function getMousedownHandler(plugin: TableSort) {
+	return async (evt: MouseEvent) => {
+
+		if (evt.target == null) {
+			return;
+		}
+		const element: HTMLElement = evt.target as HTMLElement;
+		if (element.tagName !== "TH") {
+			return;
+		}
+		const tableElement: HTMLTableElement | undefined = plugin.getTableElement(element);
+		if (!tableElement || plugin.hasCustomClasses(tableElement)) {
+			return;
+		}
+
+		evt.preventDefault();
+
+		const tableID: number = plugin.getTableID(tableElement);
+
+		let table;
+		if (plugin.isNewTable(tableID)) {
+			table = new Table(tableID, tableElement);
+			plugin.storage.push(table);
+		} else {
+			table = plugin.storage[tableID];
+		}
+
+		const columnIndex = table.getColumnIndex(element);
+		const column: Column = table.getColumnDataAt(columnIndex);
+
+		table.handleClick(column, evt.ctrlKey == true);
+		table.sort();
+	}
+}
